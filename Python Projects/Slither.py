43da3e253f584cf821e1a3d431469990b44d11b8
Koromonnnnnnnn <764363@dpsk12.net>
2023-09-25 09:04:24 -0600

    Update Slither.py

--- Python Projects/Slither.py
+++ Python Projects/Slither.py
@@ -5,13 +5,17 @@ pygame.init()
 
 
 #creates game screen and caption
-screen = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("simple base code")
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("Slither Jaime")
+clock = pygame.time.Clock()
 
 #game variables
 doExit = False #variable to quit out of game loop
 
-
+xPos = 200
+yPos = 200
+Vx = 1
+Vy = 1
 
 #BEGIN GAME LOOP######################################################
 while not doExit:
