438dd95b727921f30e7249e25ca265685225be43
Koromonnnnnnnn <764363@dpsk12.net>
2023-09-26 10:15:02 -0600

    Create cookie.py

---
+++ Python Projects/cookie.py
@@ -0,0 +1,33 @@
+#------------------------------------P2
+
+choice = "duck"
+while choice != "cookie":
+        print(choice)
+        choice = input("type cookie to quit: ")
+
+print("no more cookies. goodbye.")
+
+#------------------------------------P2
+
+num = 100
+while num > 5:
+    print(num, end = " ")
+    num-=7
+       
+#------------------------------------P3
+    
+num = 100
+while num >= 10:
+    print(num, end = " ")
+    num+=5
+
+#------------------------------------P4
+    
+counter = 0
+num = 1
+
+while num <= 50:
+    print(counter, end = " ")
+    num*=2
+    counter+=1
+
