8fb05aff641ea79bffb2a3decb92311051e77874
Koromonnnnnnnn <764363@dpsk12.net>
2023-09-27 08:27:37 -0600

    Create earbleed.cpp

---
+++ C++ Projects/earbleed.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<Windows.h> // new library
+
+using namespace std;
+
+int main(){
+    int num = 5;
+
+    //call the window title function
+    SetConsoleTitleA("My Awsome Game!");
+
+    while (num > 0) {
+
+        //call the console color function
+        system("COLOR 2F");
+
+        //call the beep function
+        Beep(500, 800);
+
+        //a new color!
+        system("COLOR 84");
+
+        //call the messagebox function
+        MessageBox(nullptr, TEXT("Python Sucks!!"), TEXT("C++ Rules."), MB_OK);
+
+        cout << "yay functions!" << endl;
+        system("pause");
+        num--; //decrease loop counter (so its not infinite)
+
+
+
+
+    } //end of while loop
+
+    return 0;
+
+} //end of main
