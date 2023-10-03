e7ae6bc8aee8db0f50b1549fcfcf48f6f364c76a
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-03 10:18:01 -0600

    Update whileloopquiz.py

--- Python Projects/whileloopquiz.py
+++ Python Projects/whileloopquiz.py
@@ -1,4 +1,4 @@
-#1
+#1 Write a python while loop that continuously says "AH CHOO!" until the user types "bless you". 
 
 password = ''
 
@@ -6,14 +6,14 @@ while password != 'bless you':
     print('AH CHOO!')
     password = input()
 
-#2
+#2 Write a python while loop that prints out the numbers 20 through 200 counting by 10s. 
 
 num = 20
 while num <= 200:
     print(num, end = " ")
     num+=10
 
-#3
+#3 Write a python while loop that prints out the number 80 and subtracts 5 until the number reaches 35, printing each number as it goes.
 
 num = 80
 while num > 35:
