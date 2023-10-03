5d91cadf21d0979d95e62d549fd8d235abaf987e
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-03 10:15:52 -0600

    Update whileloopquiz.py

---
+++ Python Projects/whileloopquiz.py
@@ -0,0 +1,21 @@
+#1
+
+password = ''
+
+while password != 'bless you':
+    print('AH CHOO!')
+    password = input()
+
+#2
+
+num = 20
+while num <= 200:
+    print(num, end = " ")
+    num+=10
+
+#3
+
+num = 80
+while num > 35:
+    print(num, end = " ")
+    num-=5
