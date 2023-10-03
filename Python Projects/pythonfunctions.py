023d101718c501edb34b8a984e05d55e0fb1d6e7
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-03 10:10:07 -0600

    python functions
    monster gen
    
    starwars beeps

---
+++ Python Projects/pythonfunctions.py
@@ -0,0 +1,17 @@
+import random
+
+def MonsterGen():
+    num = random.randrange(0,100)
+    if num < 20:
+        print("A skeleton has appeared")
+    elif num < 50:
+        print("A zombie chases you")
+    elif num < 80:
+        print("A spider attacks")
+    else:
+        print("Nothing Appeared")
+
+
+while True:
+    MonsterGen()
+    choice = input("Enter any key to continue...")
---
+++ Python Projects/starwarsbeep.py
@@ -0,0 +1,23 @@
+from winsound import Beep
+
+def StarWarsBeeps():
+    Beep(440, 500)
+    Beep(440, 500)
+    Beep(440, 500)
+    Beep(349, 350)
+    Beep(523, 150)
+    Beep(440, 500)
+    Beep(349, 350)
+    Beep(523, 150)
+    Beep(440, 1000)
+    Beep(659, 500)
+    Beep(659, 500)
+    Beep(659, 500)
+    Beep(698, 350)
+    Beep(523, 150)
+    Beep(415, 500)
+    Beep(349, 350)
+    Beep(523, 150)
+    Beep(440, 1000)
+
+StarWarsBeeps()
