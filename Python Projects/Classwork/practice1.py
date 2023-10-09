5c89aa9869758f355cfbf0b2ecf10b3220d876d3
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-09 09:40:14 -0600

    sys

--- Python Projects/Classwork/practice1.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-cases = int(sys.stdin.readline().rstrip)
-
-for i in range(cases):
-    line = sys.stdin.readline().rstrip()
-    for j in range(0, len(line)):
---
+++ Python Projects/Classwork/syspractice1.py
@@ -0,0 +1,11 @@
+import sys
+
+cases = int(sys.stdin.readline().rstrip()) #get the first number of cases
+
+for i in range(cases): #go through the other lines
+    
+    line = sys.stdin.readline().rstrip() 
+
+    line = line.split("") #split up the line by spaces
+
+    print(int(line[0])+int(line[1]), int(line[0])*int(line[1]))
