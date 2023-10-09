ce4033fe5744ad199251d941aa539329e61abb84
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-09 09:51:18 -0600

    Update syspractice1.py

--- Python Projects/Classwork/syspractice1.py
+++ Python Projects/Classwork/syspractice1.py
@@ -6,6 +6,6 @@ for i in range(cases): #go through the other lines
 
     line = sys.stdin.readline().rstrip() 
 
-    line = line.split("") #split up the line by spaces
+    line = line.split(" ") #split up the line by spaces
 
     print(int(line[0])+int(line[1]), int(line[0])*int(line[1]))
