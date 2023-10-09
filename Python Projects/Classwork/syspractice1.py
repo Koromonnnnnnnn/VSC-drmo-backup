ac28455a69de81d75f134fd7bfacd6e19f85ac65
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-09 09:49:44 -0600

    Update syspractice1.py

--- Python Projects/Classwork/syspractice1.py
+++ Python Projects/Classwork/syspractice1.py
@@ -3,7 +3,7 @@ import sys
 cases = int(sys.stdin.readline().rstrip()) #get the first number of cases
 
 for i in range(cases): #go through the other lines
-    
+
     line = sys.stdin.readline().rstrip() 
 
     line = line.split("") #split up the line by spaces
