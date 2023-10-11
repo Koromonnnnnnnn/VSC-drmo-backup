cbfddab847c9c61e089bb4ab1d3c0e2ea99d0b0b
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-11 10:22:02 -0600

    format

--- Python Projects/Projects/dinojumper.py
+++ Python Projects/Projects/dinojumper.py
@@ -3,35 +3,31 @@ import pygame
 pygame.init()
 
 
-#creates game screen and caption
+# creates game screen and caption
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("dino jumper")
 
-#game variables
-doExit = False #variable to quit out of game loop
+# game variables
+doExit = False  # variable to quit out of game loop
 clock = pygame.time.Clock()
 
 
-#BEGIN GAME LOOP######################################################
+# BEGIN GAME LOOP######################################################
 while not doExit:
-   
-   
-    #pygame's way of listening for events (key presses, mouse clicks, etc)
+
+    # pygame's way of listening for events (key presses, mouse clicks, etc)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-           doExit = True #lets you quit program
+            doExit = True  # lets you quit program
 
-    #timer
+    # timer
     clock.tick(60)
 
-    #keyboard input-----------------------------------
-  
-     
-    #render section-----------------------------------
- 
+    # keyboard input-----------------------------------
 
+    # render section-----------------------------------
 
-    pygame.display.flip() #update graphics each game loop
+    pygame.display.flip()  # update graphics each game loop
 
-#END GAME LOOP#######################################################
+# END GAME LOOP#######################################################
 pygame.quit()
--- Python Projects/Projects/lovelyhouse.py
+++ Python Projects/Projects/lovelyhouse.py
@@ -2,10 +2,10 @@ import pygame
 
 pygame.init()
 
-#creates game screen and caption
+# creates game screen and caption
 
-screen = pygame.display.set_mode((800, 800)) #create game screen
-pygame.display.set_caption("A lovely House!") #window title
+screen = pygame.display.set_mode((800, 800))  # create game screen
+pygame.display.set_caption("A lovely House!")  # window title
 
 pygame.draw.rect(screen, (200, 0, 200), (200, 400, 300, 300))
 pygame.draw.rect(screen, (255, 255, 255), (380, 350, 100, 100))
@@ -16,30 +16,27 @@ pygame.draw.circle(screen, (100, 150, 0), (150, 100), 50)
 pygame.draw.circle(screen, (230, 150, 0), (200, 150), 50)
 pygame.draw.circle(screen, (244, 150, 0), (250, 200), 50)
 pygame.draw.circle(screen, (244, 244, 244), (50, 200), 50)
-pygame.draw.polygon(screen, (0, 244, 200), ((200, 400), (500, 400), (400, 100)))
+pygame.draw.polygon(screen, (0, 244, 200),
+                    ((200, 400), (500, 400), (400, 100)))
 
 
-#game variables
-doExit = False #variable to quit out of game loop
+# game variables
+doExit = False  # variable to quit out of game loop
 
 
-
-#BEGIN GAME LOOP######################################################
+# BEGIN GAME LOOP######################################################
 while not doExit:
-   
-   
-    #pygame's way of listening for events (key presses, mouse clicks, etc)
+
+    # pygame's way of listening for events (key presses, mouse clicks, etc)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-           doExit = True #lets you quit program
+            doExit = True  # lets you quit program
 
-    #keyboard input-----------------------------------
-  
-     
-    #render section-----------------------------------
+    # keyboard input-----------------------------------
 
+    # render section-----------------------------------
 
-    pygame.display.flip() #update graphics each game loop
+    pygame.display.flip()  # update graphics each game loop
 
-#END GAME LOOP#######################################################
+# END GAME LOOP#######################################################
 pygame.quit()
