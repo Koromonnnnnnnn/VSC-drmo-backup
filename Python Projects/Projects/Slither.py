d2d2e87e708b81a011e43aaa7f6eb026b572fe15
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-20 09:26:03 -0600

    Update Slither.py

--- Python Projects/Projects/Slither.py
+++ Python Projects/Projects/Slither.py
@@ -32,7 +32,7 @@ class pellet:
 
     def draw(self):
         pygame.draw.circle(screen, (self.red, self.green,
-                           self.blue), (self.xPos, self.yPos), self.radius)
+                                    self.blue), (self.xPos, self.yPos), self.radius)
 # end class pellet
 
 # p1 = pellet(200, 50, 100, 20, 220, 10)
@@ -44,7 +44,8 @@ pelletBag = list()
 
 for i in range(10):
     pelletBag.append(pellet(random.randrange(0, 400), random.randrange(0, 400), random.randrange(0, 255), random.randrange(
-        0, 255), random.randrange(0, 255), 10))  # randomizes x,y values and puts the pellets randomly on the screen
+        # randomizes x,y values and puts the pellets randomly on the screen
+        0, 255), random.randrange(0, 255), 10))
 
 
 # BEGIN GAME LOOP######################################################
