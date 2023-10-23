e670521a5123f65b55d7dc9b46520f34088b459c
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-23 09:37:37 -0600

    Update TBGproject.cpp

--- C++ Projects/Projects/TBG/TBGproject.cpp
+++ C++ Projects/Projects/TBG/TBGproject.cpp
@@ -46,7 +46,7 @@ int main()
     int monsterHealth = 50;
     int monsterDamage = 10;
 
-    //data
+    // data
     int hasbeenRoom2 = 0;
 
     // inventory
@@ -79,7 +79,6 @@ int main()
                 {
                     playerHealth -= monsterDamage;
                     cout << monster << " attacks you for " << monsterDamage << " damage" << endl;
-
                 }
                 if (playerHealth <= 0)
                 {
@@ -151,14 +150,15 @@ int main()
             break;
         case 1:
             monsterHealth += monsterHealth; // revive monster for next fight
-            while (hasbeenRoom2 != 1){
+            while (hasbeenRoom2 != 1)
+            {
                 cout << "Press enter to heal" << endl;
                 cin.ignore();
                 cin.get();
                 playerHealth += monsterDamage; // heals player from previous fight damage
                 cout << "You have been healed!" << endl;
                 hasbeenRoom2++;
-        }
+            }
             cout << "You are in 'Room Two', do you wish to go (S)outh towards 'Room Three'? Or (W)est back to 'Room One'" << endl;
             cin >> destination;
             if (destination == 's' || destination == 'S')
