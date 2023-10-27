7a38552d96d15d911e487f3de4b16fc2d097d97f
Koromonnnnnnnn <764363@dpsk12.net>
2023-10-27 10:33:37 -0600

    Update functionsquiz2.py

--- Python Projects/Projects/Quiz/functionsquiz2.py
+++ Python Projects/Projects/Quiz/functionsquiz2.py
@@ -1,4 +1,4 @@
-#1 ========
+# 1 ========
 
 def simpleCalculator(x, y, z):
     if z == "add":
@@ -7,19 +7,21 @@ def simpleCalculator(x, y, z):
         print(x - y)
     else:
         print("Invalid")
-        
+
+
 simpleCalculator(1, 2, "add")
 
-#2 ========
+# 2 ========
+
 
 def candyDispenser(num):
     target = 0
     mnm = "ok, here are some m&m"
     snicker = "ok, here are some snicker"
     while num != target:
-        print("You have ", num, "pieces of candy left!" )
+        print("You have ", num, "pieces of candy left!")
         selection = input("Would you like snickers or m&ms?")
-        
+
         if selection == "m&ms":
             print(mnm)
             num -= 1
@@ -27,12 +29,15 @@ def candyDispenser(num):
             print(snicker)
             num -= 1
 
+
 candyDispenser(5)
 
-#3 ========
+# 3 ========
+
 
 def OobaAdder():
     selection = input("enter a word")
     print(selection+"ooba")
-    
+
+
 OobaAdder()
