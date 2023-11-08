f863155a57cd9c52e2f4b2c8ae077d5908028bee
Koromonnnnnnnn <764363@dpsk12.net>
2023-11-08 09:01:31 -0700

    Create forloopwarmup.cpp

---
+++ C++ Projects/Classwork/forloopwarmup.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+  for (int j = 40; j <= 80; j += 2)
+    cout << j <<" ";
+
+  cout << endl;
+
+  for (int k = 100; k >= 50; k -= 5)
+    cout << k << " ";
+ 
+  cout << endl;
+
+  for (int k = 200; k >= 100; k -= 10)
+    cout << k << " ";
+
+  cout << endl;
+  }
