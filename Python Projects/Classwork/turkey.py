a40f4c484f2e7b9941f300608b96d52f87f9cf73
Koromonnnnnnnn <764363@dpsk12.net>
2023-11-14 10:14:32 -0700

    Update turkey.py

--- Python Projects/Classwork/turkey.py
+++ Python Projects/Classwork/turkey.py
@@ -21,14 +21,22 @@ while not doExit:
 
     screen.fill((255, 0, 255))
 
+    #Left
     pygame.draw.circle(screen, (0, 0, 0), (500, 550), 50)
     pygame.draw.circle(screen, (100, 150, 255), (475, 600), 50)
     pygame.draw.circle(screen, (50, 90, 255), (550, 525), 50)
+    
+    #Right
+    pygame.draw.circle(screen, (0, 0, 0), (750, 550), 50)
+    pygame.draw.circle(screen, (100, 150, 255), (775, 600), 50)
+    pygame.draw.circle(screen, (50, 90, 255), (700, 525), 50)
 
+    #Eyes   
     pygame.draw.ellipse(screen, (110, 70, 0),
                         (WIDTH // 2, HEIGHT // 2, 250, 300))
     pygame.draw.ellipse(screen, (255, 70, 0), (560, 400, 125, 200))
 
+    #Eyes
     pygame.draw.circle(screen, (255, 255, 255), (600, 475), 25)
     pygame.draw.circle(screen, (255, 255, 255), (650, 475), 25)
     pygame.draw.circle(screen, (0, 0, 0), (600, 475), 15)
