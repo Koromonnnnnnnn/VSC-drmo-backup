631c95f54f81e1cc3237564cf6ee05af34c04230
Koromonnnnnnnn <764363@dpsk12.net>
2023-11-14 09:22:17 -0700

    Create turkey.py

---
+++ Python Projects/Classwork/turkey.py
@@ -0,0 +1,27 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 1000
+HEIGHT = 1000
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Turkey!")
+
+doExit = False
+
+
+while not doExit:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            doExit = True  # lets you quit program
+
+    # render section-----------------------------------
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.ellipse(screen, (110, 70, 0), (WIDTH // 2, LENGTH // 2, 200, 200))
+    pygame.display.flip()
+
+pygame.quit()
